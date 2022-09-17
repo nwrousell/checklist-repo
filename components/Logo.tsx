162676982eb3413ae810211768861f79835d7976
Noah Rousell <59492853+nwrousell@users.noreply.github.com>
2022-09-17 11:01:20 -0400

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -6,7 +6,10 @@ export function Logo() {
         <Link href="/">
             <div className="flex items-center cursor-pointer select-none">
                 <HiOutlineClipboardList size={36} className="mr-2 text-primary-700 dark:text-primary-500" />
-                <p className="font-mono text-xl font-semibold text-primary-700 dark:text-primary-500">Checklist Repo</p>
+                <div>
+                    <p className="mb-0 font-mono text-xl font-semibold text-primary-700 dark:text-primary-500">Checklist Repo</p>
+                    <p className="-mt-1 font-mono text-xs italic font-semibold text-gray-500 dark:opacity-100 dark:text-primary-700">Checklists for Creatives</p>
+                </div>
             </div>
         </Link>
     );
